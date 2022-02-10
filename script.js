b48a57d722caf30065f85e9f9086c14713fbde6d
sagarkivale101 <sagarkivale101@gmail.com>
2022-02-10 17:56:35 +0530

    simple calc

---
+++ script.js
@@ -0,0 +1,21 @@
+let screen = document.getElementById("output-screen");
+
+function disp(num) {
+  screen.value += num;
+}
+
+function calc() {
+  try {
+    screen.value = eval(screen.value);
+  } catch (error) {
+    alert(error.message);
+  }
+}
+
+function cl() {
+  screen.value = "";
+}
+
+function del() {
+  screen.value = screen.value.slice(0, -1);
+}
