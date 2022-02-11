c307c733cc5ec09e667f6d0a1a12a43ad41fa972
sagarkivale101 <sagarkivale101@gmail.com>
2022-02-11 16:35:41 +0530

    scientific 1.2

--- script.js
+++ script.js
@@ -4,11 +4,34 @@ function disp(num) {
   screen.value += num;
 }
 
+
+
 function calc() {
-  try {
-    screen.value = eval(screen.value);
-  } catch (error) {
-    alert(error.message);
+  if(screen.value.slice(0,1) == '√'){
+    
+    var x =+screen.value.slice(1,)
+    screen.value = (Math.sqrt(x));
+  }else if(screen.value.includes('^') === true){
+    var x = screen.value.indexOf('^')
+    var a = screen.value.slice(0, x);
+    var b = screen.value.slice(x+1, );
+    screen.value = Math.pow(a, b);
+
+  }else if(screen.value.slice(-1,) == '!'){
+            var a =screen.value.slice(0, -1);
+            var b = 1;
+            for(var i = 1; i <= a; i++){
+                b *= i;
+              }
+          
+        screen.value = b
+
+  }else{
+      try {
+        screen.value = eval(screen.value);
+      } catch (error) {
+        alert(error.message);
+      }
   }
 }
 
